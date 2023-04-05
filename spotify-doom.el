ad057af31428f5bfffa982177764ee1a9891ae14
(Lemon) Robert M Martinez <robert.mychal.martinez@gmail.com>
2023-04-05 04:58:21 -0400

    Merged branches locally, basically lol.
    ;; Author: Lemon Newby <lemon.newby@gmail.com>
    ;; URL: https://github.com/proverbiallemon/spotify-doom
    ;; Version: 0.1.0
    ;; Package-Requires: ((emacs "26.1"))
    ;; Keywords: multimedia, music, spotify, doom
    
    ;;; Commentary:
    
    ;; Spotify-Doom is an Emacs package that allows you to control Spotify
    ;; playback and manage playlists directly from Doom Emacs. Features include
    ;; playback control, search, playlist management, device switching, and more.
    ;;
    ;; To get started, load the package and follow the installation instructions
    ;; in the README or documentation.
    ;;
    ;; For more information and usage instructions, visit the project's GitHub
    ;; repository: https://github.com/proverbiallemon/spotify-doom
    
    ;;; Code:
    
    ;; Your package implementation goes here
    
    (provide 'spotify-doom)
    ;;; spotify-doom.el ends here

---
+++ spotify-doom.el
@@ -0,0 +1,26 @@
+;;; spotify-doom.el --- Control Spotify from Doom Emacs -*- lexical-binding: t -*-
+
+;; Author: Lemon Newby <lemon.newby@gmail.com>
+;; URL: https://github.com/proverbiallemon/spotify-doom
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: multimedia, music, spotify, doom
+
+;;; Commentary:
+
+;; Spotify-Doom is an Emacs package that allows you to control Spotify
+;; playback and manage playlists directly from Doom Emacs. Features include
+;; playback control, search, playlist management, device switching, and more.
+;;
+;; To get started, load the package and follow the installation instructions
+;; in the README or documentation.
+;;
+;; For more information and usage instructions, visit the project's GitHub
+;; repository: https://github.com/proverbiallemon/spotify-doom
+
+;;; Code:
+
+;; Your package implementation goes here
+
+(provide 'spotify-doom)
+;;; spotify-doom.el ends here
