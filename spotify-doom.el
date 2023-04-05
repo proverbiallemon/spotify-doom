c6fa78850f455c17ea77595382b818b610eb601b
(Lemon) Robert M Martinez <robert.mychal.martinez@gmail.com>
2023-04-05 06:21:24 -0400

    Added requirements to spotify-doom.el

--- spotify-doom.el
+++ spotify-doom.el
@@ -1,26 +1,41 @@
-;;; spotify-doom.el --- Control Spotify from Doom Emacs -*- lexical-binding: t -*-
+;;; spotify-doom.el --- Control Spotify from Doom Emacs -*- lexical-binding: t; -*-
 
-;; Author: Lemon Newby <lemon.newby@gmail.com>
+;; Author: Lemon Newby
 ;; URL: https://github.com/proverbiallemon/spotify-doom
-;; Version: 0.1.0
-;; Package-Requires: ((emacs "26.1"))
-;; Keywords: multimedia, music, spotify, doom
+;; Version: 0.1.1
+;; Keywords: multimedia, spotify, doom
 
-;;; Commentary:
+;; This file is not part of GNU Emacs.
 
-;; Spotify-Doom is an Emacs package that allows you to control Spotify
-;; playback and manage playlists directly from Doom Emacs. Features include
-;; playback control, search, playlist management, device switching, and more.
-;;
-;; To get started, load the package and follow the installation instructions
-;; in the README or documentation.
-;;
-;; For more information and usage instructions, visit the project's GitHub
-;; repository: https://github.com/proverbiallemon/spotify-doom
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;; Packages-Required
+;; ((emacs "26.1") (request "0.3.3") (oauth2 "0.10") (simple-httpd "1.5.1"))
+
+(require 'request)
+(require 'oauth2)
+(require 'simple-httpd)
 
 ;;; Code:
 
 ;; Your package implementation goes here
 
 (provide 'spotify-doom)
+
 ;;; spotify-doom.el ends here
